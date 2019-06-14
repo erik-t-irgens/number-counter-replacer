46e2fd2fb1eb9b6d47e9d2f988cc4e9597e63d18
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-14 14:25:27 -0700

    create working replacement for all numbers with 1, 2, or 3 inclusion. Add working front-end that counts displays one number at a time, as if it is counting in real time.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,41 @@
 $(document).ready(function() {
     $("form#counterForm").submit(function(event) {
 
+       // business code
         // This will take user input and store it
 
         var userNumber = parseInt($("#userNumber").val());
         console.log(userNumber);
 
-
-        //  This logic gathers userNumber and then creates an array full of every number below it, including zero. 
-        debugger;
+        // Start with an empty array that we'll put our results into
         var countedString = [];
 
-debugger
+        // Use a for loop to cound up to userNumber
         for (let index = 0; index <= userNumber; index++) {
-            countedString.push("" + index); // Adds the numbers 0 to userNumber via index, while making them strings instead of integers, to my array.
-            if (countedString.indexOf("3") >= 0) {
-                var replace3 = countedString[index].indexOf("3");
-                console.log(replace3);
-                console.log(countedString[replace3]);
-                countedString[index, replace3] = "I'm sorry, Dave. I'm afraid I can't do that.";
-                
-            }  
-            if (countedString.indexOf("2") >= 0) {
-                var replace2 = countedString[index].indexOf("2");
-                countedString[index, replace2]= "Boop!";
-               
-            }  
-            if (countedString.indexOf("1") >= 0) {
-                var replace1 = countedString[index].indexOf("1");
-                countedString[index, replace1] = "Beep!";
-               
+            // Convert the number to a string so we can check whether "1", "2", and "3" are in it
+            var counterStep = "" + index;
+            // Check 3 first, then 2, then 1 to cascade the rules
+            // Pushing the result to the array if it's found
+            if (counterStep.indexOf("3") >= 0) {
+                countedString.push("I'm sorry, Dave. I'm afraid I can't do that.");
             }
-        
+            else if (counterStep.indexOf("2") >= 0) {
+                countedString.push("Boop!");
+            }
+            else if (counterStep.indexOf("1") >= 0) {
+                countedString.push("Beep!");
+            } else {
+                countedString.push(counterStep)
+            }
         }
 
+       // front end code
 
+    for (let index = 0; index < countedString.length; index++) {
+            setTimeout (function timer(){
+                $("#resultant").text(countedString[index]);
+            }, index*1000 );        
+    }
 
         console.log(countedString);
 
