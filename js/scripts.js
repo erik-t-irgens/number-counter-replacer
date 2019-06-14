8d49196bc026585338025bf645f44f72b96fc587
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-14 16:37:25 -0700

    Add plenty of styling to all facets, and add an informational button that explains what is going on.

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,12 @@ $(document).ready(function() {
 
         // Start with an empty array that we'll put our results into
         var countedString = [];
-
+        var originalString = [];
         // Use a for loop to cound up to userNumber
         for (let index = 0; index <= userNumber; index++) {
             // Convert the number to a string so we can check whether "1", "2", and "3" are in it
             var counterStep = "" + index;
+            originalString.push("" + index);
             // Check 3 first, then 2, then 1 to cascade the rules
             // Pushing the result to the array if it's found
             if (counterStep.indexOf("3") >= 0) {
@@ -38,13 +39,28 @@ $(document).ready(function() {
                   setTimeout (function timer(){
                       $("#resultant").slideDown(500);
                       $("#resultant").text(countedString[index]);
+                      $("#originalArray").append("[" + originalString[index] + "] ");
+                      $("#resultantArray").append("[" + countedString[index] + "] ");
                       $("#resultant").slideUp(500);
                   }, index*1000 );     
           }
 
+        
+          $("#whatsHappening").slideDown();
+          
+          event.preventDefault();
+        });
+        $("#clearResult").click(function() {
+            $("#originalArray").text("");
+            $("#resultantArray").text("");
+        });
 
-        event.preventDefault();
-    });
+        $("#whatsHappening").click(function() {
+            $("#resultantArray").slideDown();
+            $("#whatsHappeningHidden1").slideDown();
+            $("#whatsHappeningHidden2").slideDown();
+            $("#clearResult").slideDown();
+        });
 
 
 });
