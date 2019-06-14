745bdc137ed6a3b23c96183ab53d78d012ee202c
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-14 10:04:38 -0700

    Create for loop that makes a nested array of all integers below, and including, user's input number. Attempts to convert the nested array into string values instead of integers.

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,26 @@ $(document).ready(function() {
 
 
         //  This logic gathers userNumber and then creates an array full of every number below it, including zero. 
+        debugger;
         var countedString = [];
-
+        var nestCountedString = [];
         for (let index = 0; index <= userNumber; index++) {
             countedString.push(index);
+            nestCountedString.push([index]);
         }
 
+
+
         console.log(countedString);
+        console.log(nestCountedString);
+
+    //   First attempt to change integer values within the nested array of nestCountedString into String values.
+        // nestCountedString.map(String);
+        // console.log(nestCountedString);
+   
 
 
+        console.log(converter(nestCountedString));
 
 
 
