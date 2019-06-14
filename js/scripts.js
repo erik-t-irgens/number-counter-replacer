29e0da9863b2df02cf3d49ab98f5d396d4b4cc2b
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-14 13:44:19 -0700

    Create more fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -14,26 +14,24 @@ $(document).ready(function() {
 debugger
         for (let index = 0; index <= userNumber; index++) {
             countedString.push("" + index); // Adds the numbers 0 to userNumber via index, while making them strings instead of integers, to my array.
-            // this for loop looks at each position of the array, then look inside each string to see if there's a 1 2 or 3. If there is, (>= 0 .indexOf), it will replace the entire index in the array with the required message.
-            for (let jindex = 0; jindex < countedString.length; jindex++) {
-                if (countedString.indexOf("3") >= 0) {
-                    var replace3 = countedString.indexOf("3");
-                    console.log(replace3);
-                    console.log(countedString[replace3]);
-                    countedString[replace3] = "I'm sorry, Dave. I'm afraid I can't do that.";
-                    break;
-                }  
-                if (countedString.indexOf("2") >= 0) {
-                    var replace2 = countedString.indexOf("2");
-                    countedString[replace2]= "Boop!";
-                    break;
-                }  
-                if (countedString.indexOf("1") >= 0) {
-                    var replace1 = countedString.indexOf("1");
-                    countedString[replace1] = "Beep!";
-                    break;
-                }
+            if (countedString.indexOf("3") >= 0) {
+                var replace3 = countedString[index].indexOf("3");
+                console.log(replace3);
+                console.log(countedString[replace3]);
+                countedString[index, replace3] = "I'm sorry, Dave. I'm afraid I can't do that.";
+                
+            }  
+            if (countedString.indexOf("2") >= 0) {
+                var replace2 = countedString[index].indexOf("2");
+                countedString[index, replace2]= "Boop!";
+               
+            }  
+            if (countedString.indexOf("1") >= 0) {
+                var replace1 = countedString[index].indexOf("1");
+                countedString[index, replace1] = "Beep!";
+               
             }
+        
         }
 
 
