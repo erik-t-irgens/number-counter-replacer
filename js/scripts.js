017b4f6f21877b440c501d785ebc1cbdb648e15f
Erik Irgens <erik.t.irgens@gmail.com>
2019-06-14 11:26:59 -0700

    Add functionality to replace numbers converted into strings into the correct message by using indexOf(number) >=0 to replace it. However, it replaces every single array item with the rule.

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,25 @@ $(document).ready(function() {
         //  This logic gathers userNumber and then creates an array full of every number below it, including zero. 
         debugger;
         var countedString = [];
-        var nestCountedString = [];
+
+
         for (let index = 0; index <= userNumber; index++) {
-            countedString.push(index);
-            nestCountedString.push([index]);
+            countedString.push("" + index); // Adds the numbers 0 to userNumber via index, while making them strings instead of integers, to my array.
+            // this for loop looks at each position of the array, then look inside each string to see if there's a 1 2 or 3. If there is, (>= 0 .indexOf), it will replace the entire index in the array with the required message.
+            for (let jindex = 0; jindex < countedString.length; jindex++) {
+                if (countedString.indexOf("3") >= 0) {
+                    countedString[jindex] = "I'm sorry, Dave. I'm afraid I can't do that.";
+                }  else if (countedString.indexOf("2") >= 0) {
+                    countedString[jindex]= "Boop!";
+                }  else if (countedString.indexOf("1") >= 0) {
+                    countedString[jindex] = "Beep!";
+                }
+            }
         }
 
 
 
         console.log(countedString);
-        console.log(nestCountedString);
-
-    //   First attempt to change integer values within the nested array of nestCountedString into String values.
-        // nestCountedString.map(String);
-        // console.log(nestCountedString);
-   
-
-
-        console.log(converter(nestCountedString));
 
 
 
